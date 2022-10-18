e3f5e1baac3ddf47ef9add76bc1190e28ecca520
Khoa Minh Ngo <114366700+Mkhoa161@users.noreply.github.com>
2022-10-18 11:19:28 -0600

    Create 19_solution.py

---
+++ 19_solution.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head.next == None:
+            return None
+        tmp = head
+        size = 0
+        
+        # find the size of the linked list
+        while tmp:
+            size += 1
+            tmp = tmp.next
+        tmp = head
+        
+        #if we have to remove the first node:
+        if n == size: 
+            return head.next
+        
+        for i in range(size-n-1):
+            tmp = tmp.next
+        tmp.next = tmp.next.next
+        return head
