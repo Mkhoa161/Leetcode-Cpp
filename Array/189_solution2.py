8755a6f51e4d4b3c6a8c043715d696971fa56ed5
Khoa Minh Ngo <114366700+Mkhoa161@users.noreply.github.com>
2023-03-05 12:38:35 -0700

    Create 189_solution2.py

---
+++ Array/189_solution2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k > len(nums):
+            k = k % len(nums)
+        
+        def reverse(nums, left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
+                right -= 1
+        
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
