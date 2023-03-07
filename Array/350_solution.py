4d1057c2bbe506d037ef81027d9cd0e5d77a4e59
Khoa Minh Ngo <114366700+Mkhoa161@users.noreply.github.com>
2023-03-06 17:11:48 -0700

    Create 350_solution.py

---
+++ Array/350_solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        for val in nums1:
+            if val in nums2:
+                result.append(val)
+                nums2.remove(val)
+        return result 
